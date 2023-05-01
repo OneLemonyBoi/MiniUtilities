44a2ef4195efb7d20abc27ad43260205b299dde4
MycroftJr <33704388+mycroftjr@users.noreply.github.com>
2023-04-30 20:05:33 -0700

    Update SpikeBlock.java

--- src/main/java/onelemonyboi/miniutilities/blocks/spikes/SpikeBlock.java
+++ src/main/java/onelemonyboi/miniutilities/blocks/spikes/SpikeBlock.java
@@ -114,11 +114,7 @@ public class SpikeBlock extends net.minecraft.world.level.block.Block {
         }
         else {entityIn.hurt(DamageSource.CACTUS, this.damage);}
         if (this.expDropTrue) {
-            try {
-                ReflectionUtil.setFieldValue(LivingEntity.class.getDeclaredField("lastHurtByPlayerTime"), entity, 100);
-            } catch (NoSuchFieldException e) {
-                e.printStackTrace();
-            }
+            entity.lastHurtByPlayerTime = 100;
         }
         super.stepOn(worldIn, pos, state, entityIn);
     }
